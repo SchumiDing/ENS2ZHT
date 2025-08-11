3506d2a9565d6d71be27d31782eceb5271fb82d1
SchumiDing <dingruiyi@163.com>
2025-08-11 10:02:39 +0800

    Update train.py

--- Model/train.py
+++ Model/train.py
@@ -74,6 +74,7 @@ def cosSimLoss(aM, bM):
     if len(aM) != len(bM):
         raise ValueError("Input tensors must have the same length.")
     com = 0
+    print(aM.shape, bM.shape)
     for at, bt in zip(aM, bM):
         a = at[0]
         b = bt[0]
